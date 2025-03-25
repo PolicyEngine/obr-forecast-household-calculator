f45472b82b93133dc7aa8c915bf3d8826ef7832e
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-03-25 17:15:46 +0000

    Add analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -18,6 +18,8 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/blue.svg" />
+        <script defer data-domain="obr-forecast-household-calculator-578039519715.us-central1.run.app" src="https://plausible.io/js/script.file-downloads.hash.outbound-links.pageview-props.revenue.tagged-events.js"></script>
+        <script dangerouslySetInnerHTML={{ __html: `window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }` }}></script>
       </head>
       <body className={`${inter.className} bg-gray-50`}>{children}</body>
     </html>
